bfbaa7f0f79dd55b8ce007f014747db15c5ab046
abhiman23897 <116314001+abhiman23897@users.noreply.github.com>
2024-03-03 00:23:21 -0500

    Test 4

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,3 @@
 QUERY_AUGMENTATION_PROMPT = "Give {n} different versions of the query: {original_query}"
-RAG_PROMPT = "{original_query} only using information from the relevant extracts: {documents}. Clearly highlight which extracts were used in the answer"
+RAG_PROMPT = "Answer this question : \"{original_query}\" only using information from the relevant extracts out of the following: {documents}. Clearly highlight which extracts were used in the answer"
 REWARD_PROMPT = "For this question: {original_query}, give a score out of 5 to this answer: {answer}."
