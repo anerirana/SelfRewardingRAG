f2b35b3aa19126695d1c370eeb8aebfd19806ef9
aneelakantes_umass_edu <aneelakantes@umass.edu>
2024-03-13 20:01:24 +0000

    Merge errors

--- src/llm.py
+++ src/llm.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from transformers import AutoTokenizer,AutoModelForCausalLM
+from trl import DPOTrainer
+from unsloth import FastLanguageModel
 
 class LLM(nn.Module):
     def __init__(self, model_name):
@@ -14,7 +16,7 @@ class LLM(nn.Module):
                     device_map="auto")                        
         self.model = torch.compile(self.model, mode = "max-autotune", backend="inductor")
     
-    def forward(self, prompt, temp=0.7, min_new_tokens=100):
+    def forward(self, prompt, param_dict=None):
         '''Generate response for the given prompt
 
         Parameters:
@@ -27,17 +29,29 @@ class LLM(nn.Module):
         encoded = self.tokenizer.apply_chat_template(messages, return_tensors="pt")
         inputs = encoded.to(self.model.device)     
         
-        outputs = self.model.generate(inputs, temperature=temp, top_k=0, top_p=1.0, repetition_penalty=1.4, min_new_tokens=min_new_tokens, max_new_tokens=4096, do_sample=True)
+        if param_dict is None:
+            outputs = self.model.generate(inputs, temperature=0.7, top_p=0.99, repetition_penalty=1.2, min_new_tokens=16, max_new_tokens=2048, do_sample=True)
+        else:
+            #TODO: try fixing temperature=0.7
+            outputs = self.model.generate(inputs, temperature=param_dict["temperature"], top_p=param_dict["top_p"], repetition_penalty=param_dict["repetition_penalty"], min_new_tokens=param_dict["min_new_tokens"], max_new_tokens=param_dict["max_new_tokens"], do_sample=True)
         
         return self.tokenizer.decode(outputs[0])
 
     def train(self, training_dataset, batch_size=32, num_epochs=3):
+        dpo_trainer = DPOTrainer(
+            model,
+            model_ref=None,
+            args=training_args,
+            beta=0.1,
+            train_dataset=train_dataset,
+            tokenizer=tokenizer,
+        )
+        dpo_trainer.train()
         '''Train the RAGModel on the given training_dataset
-
         Parameters:
         -----------
         training_dataset
             Contains the training data for query augemntation or reward generation or answer generator
         '''
-        
+
         pass
