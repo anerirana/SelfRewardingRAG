16829efd3133ce6768acf8210f845f4b00cbc235
anerirana <arana@umass.edu>
2024-03-31 02:04:56 +0000

    new feature: run training loop on all queries

--- indexing.py
+++ indexing.py
@@ -3,7 +3,6 @@ import requests
 import time
 from functools import partial
 from os import listdir
-from os.path import isfile, join
 from tqdm import tqdm
 
 
@@ -79,32 +78,33 @@ def read_file_as_string(file_path):
 
 
 def fetch_docs_from_text(dir_path, index_path="./credit_agreement_database", new_index=True, doc_ids=None): 
-  file_names = [f.split('.')[0] for f in listdir(dir_path)]
-  
-  if doc_ids:
-    new_file_names = [file for file in file_names if file in doc_ids]
-  file_names.extend(new_file_names)
+    file_names = [f.split('.')[0] for f in listdir(dir_path) if f.split('.')[0] != '']
+    
+    if doc_ids:
+        file_names = [file for file in file_names if file in doc_ids]
+    
+    doc_collection = []
+    for i in tqdm(range(len(file_names))):
+        file_content = read_file_as_string(dir_path + file_names[i] + '.txt')
+        doc_collection.append(file_content)
 
-  doc_collection = []
-  for i in tqdm(range(len(file_names))):
-      file_content = read_file_as_string(dir_path + file_names[i] + '.txt')
-      doc_collection.append(file_content)
-
-  print("Number of read documents: ", len(file_names))
-  if new_index:
-      RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
-      RAG.index(index_name=index_path, collection=doc_collection, document_ids=file_names)
-  else:
-      RAG = RAGPretrainedModel.from_index(index_path)
-      RAG.add_to_index(index_name=index_path, new_collection=doc_collection, new_document_ids=file_names)
-  print("Number of indexed documents: ", len(doc_collection))
+    print("Number of read documents: ", len(file_names))
+    if new_index:
+        RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
+        RAG.index(index_name=index_path, collection=doc_collection, document_ids=file_names)
+    else:
+        RAG = RAGPretrainedModel.from_index(index_path)
+        RAG.add_to_index(index_name=index_path, new_collection=doc_collection, new_document_ids=file_names)
+    print("Number of indexed documents: ", len(doc_collection))
 
 
 if __name__ == "__main__":
     dir_path = '/scratch/workspace/arana_umass_edu-goldamn_project/data/new_txt_extracts/' 
     start_time = time.time()
+    doc_ids = ['25d6ead54bf9061aedbb10b41f3fe6b40e4ce80e38a42f278b3885d94882faf1', '30e22fc99db427ca6cb1384c3f48e1bed3b349b5941264e416763828579febfa', '473f11e9beddbadbea1b4a27f44db6e1c7fc49ac134c6ce52d902b7fe166de8f', '4c2ec99f83bc81396ff37d5e7abf9880b713a61fc0d6c7b5e1fce184653e226b', '67a73b3cf3b1891210f076edaec46a82a847e9b14ef69d24cb2511bc620f1183', '8d235669280f2f0ac8d14fb1dea84bd8250cfaa6c475cc107b69f08d0d4f61b5', '9daa7f8ea8e6c0ed9294f2e2355ffc74d5badfdae430133ef556d0404f697cf1', 'aecb1c3e851cce69ba6e66726c0d345fce3efddd9c6ea7c8d2cccd91e1d13620']
     fetch_docs_from_text(dir_path, 
-    index_path="/scratch/workspace/arana_umass_edu-goldamn_project/credit_agreement_database_val_and_test")
+    index_path="/scratch/workspace/arana_umass_edu-goldamn_project/credit_agreement_database_8",
+    doc_ids=doc_ids)
     end_time = time.time()
-    execution_time = end_time - start_time
-    print(f"Execution time: {execution_time} seconds")
+    execution_time = (end_time - start_time)/60
+    print(f"Execution time: {execution_time} minutes")
