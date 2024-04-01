34d219442760543444db6845dc7d6bdf9f01aaca
Samveg Shah <68071531+Samveg12@users.noreply.github.com>
2024-04-01 19:27:17 -0400

    Update llm.py

--- src/llm.py
+++ src/llm.py
@@ -1,10 +1,10 @@
-
 import torch
 import torch.nn as nn
 from transformers import AutoTokenizer,AutoModelForCausalLM, TrainingArguments
 from trl import DPOTrainer
 from unsloth import FastLanguageModel, PatchDPOTrainer,unsloth_save_model
 from datasets import Dataset
+import transformers
 
 class LLM(nn.Module):
     def __init__(self, model_name):
@@ -12,6 +12,8 @@ class LLM(nn.Module):
         '''Retrival Augmented Generation model to generate responses for a given query 
         and a set of retieved documents
         '''
+        self.max_seq_length = 4096
+        self.max_new_tokens = 4096
         # self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         # self.model = AutoModelForCausalLM.from_pretrained(model_name,
         #             trust_remote_code=True,
@@ -21,47 +23,42 @@ class LLM(nn.Module):
         max_seq_length = 4096 # Choose any! We auto support RoPE Scaling internally!
         dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
         load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
-        model, self.tokenizer = FastLanguageModel.from_pretrained(
-                model_name = model_name,
-                max_seq_length = max_seq_length,
-                dtype = None,
-                load_in_4bit = True,
-            )
+        self.model = transformers.AutoModelForCausalLM.from_pretrained("Nexusflow/Starling-LM-7B-beta")
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained("Nexusflow/Starling-LM-7B-beta")
+
+
+
 
         # Do model patching and add fast LoRA weights
-        self.model = FastLanguageModel.get_peft_model(
-            model,
-            r = 64, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
-            target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
-                            "gate_proj", "up_proj", "down_proj",],
-            lora_alpha = 64,
-            lora_dropout = 0, # Currently only supports dropout = 0
-            bias = "none",    # Currently only supports bias = "none"
-            use_gradient_checkpointing = True,
-            random_state = 3407,
-            use_rslora = False,  # We support rank stabilized LoRA
-            loftq_config = None, # And LoftQ
-        )
+        # self.model = FastLanguageModel.get_peft_model(
+        #     model,
+        #     r = 64, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        #     target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+        #                     "gate_proj", "up_proj", "down_proj",],
+        #     lora_alpha = 64,
+        #     lora_dropout = 0, # Currently only supports dropout = 0
+        #     bias = "none",    # Currently only supports bias = "none"
+        #     use_gradient_checkpointing = True,
+        #     random_state = 3407,
+        #     use_rslora = False,  # We support rank stabilized LoRA
+        #     loftq_config = None, # And LoftQ
+        # )
     
     def forward(self, prompt, param_dict=None):
-        '''Generate response for the given prompt
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(device)
 
-        Parameters:
-        -----------
-        prompt
-            The prompt used as input to the generative model.
-            Could be a RAG or QA or reward prompt.
-        '''
-        messages = [{"role":"user", "content":prompt}]
-        encoded = self.tokenizer.apply_chat_template(messages, return_tensors="pt")
-        inputs = encoded.to(self.model.device)     
+        input_ids = self.tokenizer.encode(prompt, return_tensors="pt")
+        input_ids = input_ids.to(device)
         
-        if param_dict is None:
-            outputs = self.model.generate(inputs, pad_token_id=self.tokenizer.eos_token_id, temperature=0.7, top_p=0.99, repetition_penalty=1.2, min_new_tokens=16, max_new_tokens=2048, do_sample=True)
-        else:
-            outputs = self.model.generate(inputs, pad_token_id=self.tokenizer.eos_token_id, temperature=param_dict["temperature"], top_p=param_dict["top_p"], repetition_penalty=param_dict["repetition_penalty"], min_new_tokens=param_dict["min_new_tokens"], max_new_tokens=param_dict["max_new_tokens"], do_sample=True)
-        
-        return self.tokenizer.decode(outputs[0])
+        # Ensure the total length does not exceed max_seq_length
+        print("input_ids.shape[1]:",input_ids.shape[1])
+        max_length = max(self.max_seq_length, input_ids.shape[1]+1)
+        print("max_length:", max_length)
+
+        output = self.model.generate(input_ids=input_ids, max_length=max_length)
+        generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
+        return generated_text
 
     def train(self, epoch, training_dataset, batch_size=32, num_epochs=3):
         dataset = Dataset.from_dict(training_dataset)
@@ -96,4 +93,3 @@ class LLM(nn.Module):
         dpo_trainer.train()
         unsloth_save_model(self.model, self.tokenizer, "output/model_epoch_" + str(epoch), push_to_hub=False, token=None)
         print(">>"*40 + " END TRAINING " + ">>"*40)
-
